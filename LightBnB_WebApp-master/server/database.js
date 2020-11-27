733d8f9027b8c61344b3e48ec5b66f2b93f00010
emtupp <emilyc.tupper@gmail.com>
2020-11-27 00:17:28 +0000

    add property search quesries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -98,14 +98,68 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
-  .then(res => {
-    console.log(res.rows);
-    return res.rows;
-  });
+  const queryParams = [];
+  let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON properties.id = property_id
+    `;
+    
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE properties.city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`%${options.owner_id}%`);
+    if (queryParams.length = 1) {
+      queryString += `WHERE properties.owner_id LIKE $${queryParams.length} `;
+    } else {
+    queryString += `AND properties.owner_id LIKE $${queryParams.length} `;
+    }
+  } 
+  // else {
+  //   if (queryParams.length = 0) {
+  //     queryString += `WHERE properties.owner_id = 2 `;
+  //   } else {
+  //   queryString += `AND properties.owner_id = 2 `;
+  //   }
+  // } ** This code is only meant for testing, as there is not a form to search by owner_id.
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}`);
+    if (queryParams.length = 1) {
+      queryString += `WHERE properties.cost_per_night >= $${queryParams.length} `;
+    } else {
+    queryString += `AND properties.cost_per_night >= $${queryParams.length} `;
+    }
+  }
+  
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night}`);
+    if (queryParams.length = 1) {
+      queryString += `WHERE properties.cost_per_night <= $${queryParams.length} `;
+    } else {
+    queryString += `AND properties.cost_per_night <= $${queryParams.length} `;
+    }
+  }
+
+  queryString += `GROUP BY properties.id `
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `HAVING (avg(property_reviews.rating)) >= $${queryParams.length}`
+  }
+
+  queryParams.push(limit);
+  queryString += `
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+
+  console.log(queryString, queryParams);
+  return pool.query(queryString, queryParams)
+  .then(res => res.rows);
 }
 exports.getAllProperties = getAllProperties;
 
